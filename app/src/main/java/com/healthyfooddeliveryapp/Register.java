13735a2261d06f00d19586a23165c569402d82ac
Gutu Ivan <gutsu.ivan@mail.ru>
2022-03-16 13:07:08 +0200

    Edited views in sign_up Added new icons

---
+++ app/src/main/java/com/healthyfooddeliveryapp/Register.java
@@ -0,0 +1,14 @@
+package com.healthyfooddeliveryapp;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity {
+    EditText mFullName, mEmail, mPassword;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+    Firebase fAuth
+}
